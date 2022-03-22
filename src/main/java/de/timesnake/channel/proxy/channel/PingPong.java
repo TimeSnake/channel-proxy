e4c4a5614ba9632bdc04ef6e5c72ef0d700f90dc
funzter <cavehive@protonmail.com>
2022-03-23 00:43:02 +0100

    dividing in host and server, host registration, socket ip changed, clean up

--- src/main/java/de/timesnake/channel/proxy/channel/PingPong.java
+++ src/main/java/de/timesnake/channel/proxy/channel/PingPong.java
@@ -1,33 +1,41 @@
 package de.timesnake.channel.proxy.channel;
 
+import de.timesnake.channel.core.Host;
 import de.timesnake.channel.core.NetworkChannel;
 import de.timesnake.channel.proxy.listener.ChannelTimeOutListener;
 import de.timesnake.channel.util.message.ChannelPingMessage;
 import de.timesnake.channel.util.message.MessageType;
+import de.timesnake.library.basic.util.Tuple;
 
 import java.util.Collection;
 import java.util.HashSet;
 
 public class PingPong {
 
-    protected final Collection<Integer> pingedPorts = new HashSet<>();
+    protected final Collection<Tuple<Integer, Host>> pingedHosts = new HashSet<>();
 
     public void ping(Collection<Integer> ports) {
         for (Integer port : ports) {
-            pingedPorts.add(port);
-            NetworkChannel.getChannel().sendMessage(port, new ChannelPingMessage(port, MessageType.Ping.PING));
+            Host host = ((Channel) NetworkChannel.getChannel()).getHostByServerPort(port);
+
+            if (host == null) {
+                continue;
+            }
+
+            pingedHosts.add(new Tuple<>(port, host));
+            NetworkChannel.getChannel().sendMessage(host, new ChannelPingMessage(port, MessageType.Ping.PING));
         }
     }
 
     public void checkPong() {
-        for (Integer port : this.pingedPorts) {
-            ((Channel) NetworkChannel.getChannel()).handleServerUnregister(port);
+        for (Tuple<Integer, Host> server : this.pingedHosts) {
+            ((Channel) NetworkChannel.getChannel()).handleServerUnregister(server.getA(), server.getB());
         }
-        for (Integer port : this.pingedPorts) {
+        for (Tuple<Integer, Host> server : this.pingedHosts) {
             for (ChannelTimeOutListener listener : ((Channel) NetworkChannel.getChannel()).timeOutListeners) {
-                listener.onServerTimeOut(port);
+                listener.onServerTimeOut(server.getA());
             }
         }
-        this.pingedPorts.clear();
+        this.pingedHosts.clear();
     }
 }
