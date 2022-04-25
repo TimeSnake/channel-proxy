034dabd3364391286c04c686060bd16c275fb3d5
funzter <funzter@mail.timesnake.de>
2022-04-25 11:55:39 +0200

    changed message-socket to per-server-socket, fixed log

--- src/main/java/de/timesnake/channel/proxy/main/ChannelProxy.java
+++ src/main/java/de/timesnake/channel/proxy/main/ChannelProxy.java
@@ -1,8 +1,10 @@
 package de.timesnake.channel.proxy.main;
 
+import de.timesnake.channel.core.ChannelLogger;
 import de.timesnake.channel.core.NetworkChannel;
 import de.timesnake.channel.core.SyncRun;
 import de.timesnake.channel.proxy.channel.Channel;
+import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.plugin.Plugin;
 
 import java.util.concurrent.TimeUnit;
@@ -14,15 +16,24 @@ public class ChannelProxy extends Plugin {
     @Override
     public void onEnable() {
         plugin = this;
-        System.out.println("[Channel] Loaded network-channel");
         ChannelProxy.start(25565);
     }
 
     public static void start(Integer port) {
-        NetworkChannel.start(new Channel(Thread.currentThread(), port, port) {
+        NetworkChannel.start(new Channel(Thread.currentThread(), port, port, new ChannelLogger() {
+            @Override
+            public void printInfo(String msg) {
+                ProxyServer.getInstance().getLogger().info("[Channel] " + msg);
+            }
+
+            @Override
+            public void printWarning(String msg) {
+                ProxyServer.getInstance().getLogger().warning("[Channel] " + msg);
+            }
+        }) {
             @Override
             public void runSync(SyncRun syncRun) {
-                getPlugin().getProxy().getScheduler().schedule(getPlugin(), () -> syncRun.run(), 0, TimeUnit.MILLISECONDS);
+                getPlugin().getProxy().getScheduler().schedule(getPlugin(), syncRun::run, 0, TimeUnit.MILLISECONDS);
             }
         });
     }
