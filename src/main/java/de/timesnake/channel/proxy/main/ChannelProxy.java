1d924c29fe5c853a9c60fb20f81f34340aa7c3c9
funzter <funzter@mail.timesnake.de>
2022-07-12 20:47:13 +0200

    update to velocity

--- src/main/java/de/timesnake/channel/proxy/main/ChannelProxy.java
+++ src/main/java/de/timesnake/channel/proxy/main/ChannelProxy.java
@@ -1,44 +1,57 @@
 package de.timesnake.channel.proxy.main;
 
+import com.google.inject.Inject;
+import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
+import com.velocitypowered.api.plugin.Plugin;
+import com.velocitypowered.api.proxy.ProxyServer;
 import de.timesnake.channel.core.ChannelLogger;
 import de.timesnake.channel.core.NetworkChannel;
 import de.timesnake.channel.core.SyncRun;
 import de.timesnake.channel.proxy.channel.Channel;
-import net.md_5.bungee.api.ProxyServer;
-import net.md_5.bungee.api.plugin.Plugin;
 
-import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
 
-public class ChannelProxy extends Plugin {
-
-    private static ChannelProxy plugin;
-
-    @Override
-    public void onEnable() {
-        plugin = this;
-        ChannelProxy.start(25565);
-    }
+@Plugin(id = "channel-proxy", name = "ChannelProxy", version = "1.0-SNAPSHOT",
+        url = "https://git.timesnake.de", authors = {"MarkusNils"})
+public class ChannelProxy {
 
     public static void start(Integer port) {
         NetworkChannel.start(new Channel(Thread.currentThread(), port, port, new ChannelLogger() {
             @Override
             public void printInfo(String msg) {
-                ProxyServer.getInstance().getLogger().info("[Channel] " + msg);
+                logger.info("[Channel] " + msg);
             }
 
             @Override
             public void printWarning(String msg) {
-                ProxyServer.getInstance().getLogger().warning("[Channel] " + msg);
+                logger.warning("[Channel] " + msg);
             }
         }) {
             @Override
             public void runSync(SyncRun syncRun) {
-                getPlugin().getProxy().getScheduler().schedule(getPlugin(), syncRun::run, 0, TimeUnit.MILLISECONDS);
+                server.getScheduler().buildTask(getPlugin(), syncRun::run).schedule();
             }
         });
     }
 
     public static ChannelProxy getPlugin() {
         return plugin;
+    }
+
+    private static ChannelProxy plugin;
+    private static ProxyServer server;
+    private static Logger logger;
+
+    @Inject
+    public ChannelProxy(ProxyServer server, Logger logger) {
+        ChannelProxy.server = server;
+        ChannelProxy.logger = logger;
+    }
+
+    @Subscribe
+    public void onProxyInitialization(ProxyInitializeEvent event) {
+        plugin = this;
+        ChannelProxy.start(25565);
     }
 }
