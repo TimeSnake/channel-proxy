f046c1193165e94997e0e029d75e7a97777a7d45
funzter <cavehive@protonmail.com>
2024-09-06 00:53:25 +0200

    change gradle dsl to kotlin

---
+++ build.gradle.kts
@@ -0,0 +1,90 @@
+import kotlin.io.path.name
+
+plugins {
+    id("java")
+    id("java-base")
+    id("java-library")
+    id("maven-publish")
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+}
+
+
+group = "de.timesnake"
+version = "5.0.0"
+var projectId = 47
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+    maven {
+        url = uri("https://repo.papermc.io/repository/maven-public/")
+    }
+    maven {
+        url = uri("https://git.timesnake.de/api/v4/groups/7/-/packages/maven")
+        name = "timesnake"
+        credentials(PasswordCredentials::class)
+    }
+}
+
+dependencies {
+    implementation("de.timesnake:channel-api:5.+")
+
+    compileOnly("de.timesnake:library-basic:2.+")
+
+    compileOnly("org.jetbrains:annotations:23.0.0")
+    compileOnly("org.apache.logging.log4j:log4j-api:2.22.1")
+    compileOnly("org.apache.logging.log4j:log4j-core:2.22.1")
+
+    compileOnly("com.velocitypowered:velocity-api:3.1.1")
+    annotationProcessor("com.velocitypowered:velocity-api:3.1.1")
+}
+
+configurations.configureEach {
+    resolutionStrategy.dependencySubstitution {
+        if (project.parent != null) {
+            substitute(module("de.timesnake:channel-api")).using(project(":channel:channel-api"))
+            substitute(module("de.timesnake:library-basic")).using(project(":libraries:library-basic"))
+        }
+    }
+}
+
+tasks.register<Copy>("exportAsPlugin") {
+    from(layout.buildDirectory.file("libs/${project.name}-${project.version}-all.jar"))
+    into(findProperty("timesnakePluginDir") ?: "")
+
+    dependsOn("shadowJar")
+}
+
+tasks.withType<PublishToMavenRepository> {
+    finalizedBy("shadowJar")
+}
+
+publishing {
+    repositories {
+        maven {
+            url = uri("https://git.timesnake.de/api/v4/projects/$projectId/packages/maven")
+            name = "timesnake"
+            credentials(PasswordCredentials::class)
+        }
+    }
+
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+        }
+    }
+}
+
+tasks {
+    compileJava {
+        options.encoding = "UTF-8"
+        options.release = 21
+    }
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+    withSourcesJar()
+}
